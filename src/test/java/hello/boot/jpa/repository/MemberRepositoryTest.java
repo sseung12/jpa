10da463ca4e7c0250ed6c4fb5f2c531b36a4452a
sseung12 <pon02129@naver.com>
2022-02-09 16:13:48 +0900

    error fix

--- src/test/java/hello/boot/jpa/repository/MemberRepositoryTest.java
+++ src/test/java/hello/boot/jpa/repository/MemberRepositoryTest.java
@@ -1,37 +1,35 @@
 package hello.boot.jpa.repository;
 
-import com.fasterxml.jackson.databind.deser.std.StdKeyDeserializer;
 import hello.boot.jpa.domain.Member;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
-@WebAppConfiguration
-@ExtendWith(SpringExtension.class)
+
+@ExtendWith(MockitoExtension.class)
 @SpringBootTest
+@Transactional
 class MemberRepositoryTest {
-    @Autowired MemberRepository memberRepository;
 
+
+    @Autowired
+    private MemberRepository memberRepository;
     @Test
-    @Transactional
-    public void testMember() throws Exception{
+    void memberTest() {
 
-        Member member = new Member();
-        member.setUsername("lss");
-        Long saveId = memberRepository.save(member);
+        Member member= new Member();
+        member.setUsername("ss");
+        memberRepository.save(member);
+        Member findMember = memberRepository.find(member.getId());
 
-
-        Member findMember =memberRepository.find(saveId);
-
-        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
-        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
-
-
+        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
     }
+
+
 }
