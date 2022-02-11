03c34cba98c46cfd75839974f4fc0cf3d013e339
sseung12 <pon02129@naver.com>
2022-02-12 03:53:13 +0900

    make business logics

---
+++ src/main/java/hello/boot/jpa/service/ItemService.java
@@ -0,0 +1,33 @@
+package hello.boot.jpa.service;
+
+import hello.boot.jpa.domain.Item;
+import hello.boot.jpa.repository.ItemRepository;
+import hello.boot.jpa.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class ItemService {
+
+
+    private final ItemRepository itemRepository;
+
+    @Transactional
+    public void saveItem(Item item) {
+        itemRepository.save(item);
+    }
+
+    public Item findOne(Long id) {
+        return  itemRepository.findOne(id);
+    }
+
+    public List<Item> findAll() {
+        return  itemRepository.findAll();
+    }
+
+}
